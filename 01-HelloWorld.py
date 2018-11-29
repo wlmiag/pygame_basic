82288f1a0b9f2a443a71f1cffd41b2b99baf86aa
wlmiag <39036193@qq.com>
2018-11-29 09:40:11 +0800

    p

--- 01-HelloWorld.py
+++ 01-HelloWorld.py
@@ -3,11 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
-
+pygame.display.set_caption('Hello Pygame World!')
 while True:  # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.update()
